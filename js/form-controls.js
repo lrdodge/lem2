767e19f8730a125024209cc6367fdf9a7ebde05e
Luke Dodge <luke.r.dodge@gmail.com>
2016-11-19 12:24:42 -0600

    Add parse result messages

--- js/form-controls.js
+++ js/form-controls.js
@@ -1,12 +1,45 @@
 var FormController = (function () {
 
   var _addEventHandlers = function () {
-    $("#process-data").click(processData);
+    $("#process-data").click(parseData);
   };
 
-  var processData = function() {
+  var parseData = function() {
+    var parseResultMessage = $("#data-parse-result-message");
+    parseResultMessage.empty();
+    $("#data-parse-error-list").remove();
+
     var csv = $("#data-input").val();
     var data = Papa.parse(csv);
+
+    parseResultMessage.toggleClass("text-danger", data.errors.length > 0);
+    parseResultMessage.toggleClass("text-success", !data.errors.length);
+
+    if (!data.errors.length) {
+      parseResultMessage.text("Data Input Successful");
+      return;
+    }
+
+    parseResultMessage.text("Data Input Errors");
+
+    var errorList = $("<ul />", {
+      "id": "data-parse-error-list"
+    });
+    data.errors.forEach(function(error) {
+        console.error(error);
+        var row = $("<span/>", {
+          "text": error.row,
+          "class": "badge"
+        })
+        var errorMessage = error.code + ": " + error.message;
+
+        var errorItem = $("<li />", {
+          "text": errorMessage
+        });
+        errorItem.prepend(row);
+        errorList.append(errorItem);
+    })
+    $("#data-parse-result").append(errorList);
   }
 
   return {
