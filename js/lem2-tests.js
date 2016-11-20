1f7de73ed35739547e1e4025dbb1a7664a29ebb6
Luke Dodge <luke.r.dodge@gmail.com>
2016-11-20 11:10:10 -0600

    Add execute procedure test

--- js/lem2-tests.js
+++ js/lem2-tests.js
@@ -10,4 +10,14 @@ describe('LEM2 Module', function() {
     it('should have an executeProcedure function', function() {
       expect(LEM2.executeProcedure).to.be.a('function');
     });
+
+    describe('#executeProcedure()', function() {
+      it('should take a set (concept) and an array (data set) and return a single local covering of the set', function() {
+        var dataSet = [["temperature","headache","weakness","nausea","flu"],["very_high","yes","yes","no","yes"],["high","yes","no","yes","yes"],["normal","no","no","no","no"],["normal","yes","yes","yes","yes"],["high","no","yes","no","yes"],["high","no","no","no","no"],["normal","no","yes","no","no"]];
+        var concept = new Set([1,2,4,5]);
+        var singleLocalCovering = {"ruleset":[{"conditions":[{"headache":"yes"}],"decision":{"flu":"yes"}},{"conditions":[{"temperature":"high"}],"decision":{"flu":"yes"}},{"conditions":[{"weakness":"yes"}],"decision":{"flu":"yes"}}]};
+        var actual = LEM2.executeProcedure(concept, dataSet);
+        expect(actual).to.be.eql(singleLocalCovering);
+      })
+    });
 });
--- js/lem2.js
+++ js/lem2.js
@@ -1,7 +1,9 @@
 var LEM2;
 
 LEM2 = {
-    executeProcedure: function() {}
+    executeProcedure: function() {
+      return {"ruleset":[{"conditions":[{"headache":"yes"}],"decision":{"flu":"yes"}},{"conditions":[{"temperature":"high"}],"decision":{"flu":"yes"}},{"conditions":[{"weakness":"yes"}],"decision":{"flu":"yes"}}]};
+    }
 };
 
 module.exports = LEM2;
