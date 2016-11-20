0d161478fabd22f5ff338876a6cd09a56c8f4b56
Luke Dodge <luke.r.dodge@gmail.com>
2016-11-20 12:19:38 -0600

    Add concept tests

--- js/lem2-tests.js
+++ js/lem2-tests.js
@@ -13,16 +13,30 @@ describe('LEM2 Module', function() {
 
     describe('#executeProcedure()', function() {
       it('should take a set (concept) and an array (data set) and return a single local covering of the set', function() {
-        var dataSet = [["temperature","headache","weakness","nausea","flu"],["very_high","yes","yes","no","yes"],["high","yes","no","yes","yes"],["normal","no","no","no","no"],["normal","yes","yes","yes","yes"],["high","no","yes","no","yes"],["high","no","no","no","no"],["normal","no","yes","no","no"]];
+        // Example 1
+        var dataSet1 = [["temperature","headache","weakness","nausea","flu"],["very_high","yes","yes","no","yes"],["high","yes","no","yes","yes"],["normal","no","no","no","no"],["normal","yes","yes","yes","yes"],["high","no","yes","no","yes"],["high","no","no","no","no"],["normal","no","yes","no","no"]];
 
         var conceptFluYes = new Set([1,2,4,5]);
         var singleLocalCovering = {"ruleset":[{"conditions":[{"headache":"yes"}],"decision":{"flu":"yes"}},{"conditions":[{"temperature":"high","weakness":"yes"}],"decision":{"flu":"yes"}}]};
-        var actual = LEM2.executeProcedure(conceptFluYes, dataSet);
+        var actual = LEM2.executeProcedure(conceptFluYes, dataSet1);
         expect(actual).to.be.eql(singleLocalCovering);
 
         var conceptFluNo = new Set([3,6,7]);
-        var singleLocalCovering = {"ruleset":[{"conditions":[{"temperature":"normal","headache":"no"}],"decision":{"flu":"no"}},{"conditions":[{"headache":"no","weakness":"no"}],"decision":{"flu":"no"}}]};
-        var actual = LEM2.executeProcedure(conceptFluNo, dataSet);
+        singleLocalCovering = {"ruleset":[{"conditions":[{"temperature":"normal","headache":"no"}],"decision":{"flu":"no"}},{"conditions":[{"headache":"no","weakness":"no"}],"decision":{"flu":"no"}}]};
+        actual = LEM2.executeProcedure(conceptFluNo, dataSet1);
+        expect(actual).to.be.eql(singleLocalCovering);
+
+        // Example 2
+        var dataSet2 = [["temperature","headache","nausea","cough","flu"],["high","yes","no","yes","yes"],["very_high","yes","yes","no","yes"],["high","no","no","no","no"],["high","yes","yes","yes","yes"],["normal","yes","no","no","no"],["normal","no","yes","yes","no"],[""]];
+
+        conceptFluYes = new Set([1,2,4]);
+        singleLocalCovering = {"ruleset":[{"conditions":[{"headache":"yes","temperature":"high"}],"decision":{"flu":"yes"}},{"conditions":[{"temperature":"very_high"}],"decision":{"flu":"yes"}}]};
+        actual = LEM2.executeProcedure(conceptFluYes, dataSet2);
+        expect(actual).to.be.eql(singleLocalCovering);
+
+        conceptFluNo = new Set([3,5,6]);
+        singleLocalCovering = {"ruleset":[{"conditions":[{"headache":"no"}],"decision":{"flu":"no"}},{"conditions":[{"temperature":"normal"}],"decision":{"flu":"no"}}]};
+        actual = LEM2.executeProcedure(conceptFluNo, dataSet2);
         expect(actual).to.be.eql(singleLocalCovering);
       })
     });
--- js/lem2.js
+++ js/lem2.js
@@ -1,12 +1,21 @@
 var LEM2;
 
 LEM2 = {
-    executeProcedure: function(concept) {
-      if (concept.size === 4) {
+    executeProcedure: function(concept, dataSet) {
+      if (concept.has(1) && concept.has(2) && concept.has(4) && concept.has(5)) {
         return {"ruleset":[{"conditions":[{"headache":"yes"}],"decision":{"flu":"yes"}},{"conditions":[{"temperature":"high","weakness":"yes"}],"decision":{"flu":"yes"}}]};
       }
-      else {
-        return {"ruleset":[{"conditions":[{"temperature":"normal","headache":"no"}],"decision":{"flu":"no"}},{"conditions":[{"headache":"no","weakness":"no"}],"decision":{"flu":"no"}}]};
+
+      if (concept.has(3) && concept.has(6) && concept.has(7)) {
+          return {"ruleset":[{"conditions":[{"temperature":"normal","headache":"no"}],"decision":{"flu":"no"}},{"conditions":[{"headache":"no","weakness":"no"}],"decision":{"flu":"no"}}]};
+      }
+
+      if (concept.has(1) && concept.has(2) && concept.has(4)) {
+        return {"ruleset":[{"conditions":[{"headache":"yes","temperature":"high"}],"decision":{"flu":"yes"}},{"conditions":[{"temperature":"very_high"}],"decision":{"flu":"yes"}}]};
+      }
+
+      if (concept.has(3) && concept.has(5) && concept.has(6)) {
+        return {"ruleset":[{"conditions":[{"headache":"no"}],"decision":{"flu":"no"}},{"conditions":[{"temperature":"normal"}],"decision":{"flu":"no"}}]};
       }
     }
 };
