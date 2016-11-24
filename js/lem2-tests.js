0da022bc3e87bc7581d15cf79310a9fb7dc4c9d7
Luke Dodge <luke.r.dodge@gmail.com>
2016-11-24 11:11:02 -0600

    Refactor tests

--- js/lem2-tests.js
+++ js/lem2-tests.js
@@ -2,6 +2,18 @@
 var expect = require('chai').expect;
 var LEM2 = require('./lem2.js');
 
+// Data Example 1
+
+var dataSet1 = [["temperature","headache","weakness","nausea","flu"],["very_high","yes","yes","no","yes"],["high","yes","no","yes","yes"],["normal","no","no","no","no"],["normal","yes","yes","yes","yes"],["high","no","yes","no","yes"],["high","no","no","no","no"],["normal","no","yes","no","no"]];
+var rulesetFluYes1 = {"ruleset":[{"conditions":[{"headache":"yes"}],"decision":{"flu":"yes"}},{"conditions":[{"temperature":"high","weakness":"yes"}],"decision":{"flu":"yes"}}]};
+var rulesetFluNo1 = {"ruleset":[{"conditions":[{"temperature":"normal","headache":"no"}],"decision":{"flu":"no"}},{"conditions":[{"headache":"no","weakness":"no"}],"decision":{"flu":"no"}}]}
+
+// Data Example 2
+
+var dataSet2 = [["temperature","headache","nausea","cough","flu"],["high","yes","no","yes","yes"],["very_high","yes","yes","no","yes"],["high","no","no","no","no"],["high","yes","yes","yes","yes"],["normal","yes","no","no","no"],["normal","no","yes","yes","no"]];
+var rulesetFluYes2 = {"ruleset":[{"conditions":[{"headache":"yes","temperature":"high"}],"decision":{"flu":"yes"}},{"conditions":[{"temperature":"very_high"}],"decision":{"flu":"yes"}}]};
+var rulesetFluNo2 = {"ruleset":[{"conditions":[{"headache":"no"}],"decision":{"flu":"no"}},{"conditions":[{"temperature":"normal"}],"decision":{"flu":"no"}}]};
+
 describe('LEM2 Module', function() {
     it('should exist', function() {
         expect(LEM2).to.not.be.undefined;
@@ -15,33 +27,27 @@ describe('LEM2 Module', function() {
       expect(LEM2.reduceRuleset).to.be.a('function');
     });
 
-    describe('#executeProcedure()', function() {
-      it('should take a set (concept) and an array (data set) and return a single local covering of the set', function() {
-        // Example 1
-        var dataSet1 = [["temperature","headache","weakness","nausea","flu"],["very_high","yes","yes","no","yes"],["high","yes","no","yes","yes"],["normal","no","no","no","no"],["normal","yes","yes","yes","yes"],["high","no","yes","no","yes"],["high","no","no","no","no"],["normal","no","yes","no","no"]];
+    // Functions
 
+    describe('#executeProcedure()', function() {
+      it('should take a set (concept) and an array (data set) and return a ruleset object (single local covering of the set)', function() {
+        // Example 1
         var conceptFluYes = new Set([1,2,4,5]);
-        var singleLocalCovering = {"ruleset":[{"conditions":[{"headache":"yes"}],"decision":{"flu":"yes"}},{"conditions":[{"temperature":"high","weakness":"yes"}],"decision":{"flu":"yes"}}]};
         var actual = LEM2.executeProcedure(conceptFluYes, dataSet1);
-        expect(actual).to.be.eql(singleLocalCovering);
+        expect(actual).to.be.eql(rulesetFluYes1);
 
         var conceptFluNo = new Set([3,6,7]);
-        singleLocalCovering = {"ruleset":[{"conditions":[{"temperature":"normal","headache":"no"}],"decision":{"flu":"no"}},{"conditions":[{"headache":"no","weakness":"no"}],"decision":{"flu":"no"}}]};
         actual = LEM2.executeProcedure(conceptFluNo, dataSet1);
-        expect(actual).to.be.eql(singleLocalCovering);
+        expect(actual).to.be.eql(rulesetFluNo1);
 
         // Example 2
-        var dataSet2 = [["temperature","headache","nausea","cough","flu"],["high","yes","no","yes","yes"],["very_high","yes","yes","no","yes"],["high","no","no","no","no"],["high","yes","yes","yes","yes"],["normal","yes","no","no","no"],["normal","no","yes","yes","no"],[""]];
-
         conceptFluYes = new Set([1,2,4]);
-        singleLocalCovering = {"ruleset":[{"conditions":[{"headache":"yes","temperature":"high"}],"decision":{"flu":"yes"}},{"conditions":[{"temperature":"very_high"}],"decision":{"flu":"yes"}}]};
         actual = LEM2.executeProcedure(conceptFluYes, dataSet2);
-        expect(actual).to.be.eql(singleLocalCovering);
+        expect(actual).to.be.eql(rulesetFluYes2);
 
         conceptFluNo = new Set([3,5,6]);
-        singleLocalCovering = {"ruleset":[{"conditions":[{"headache":"no"}],"decision":{"flu":"no"}},{"conditions":[{"temperature":"normal"}],"decision":{"flu":"no"}}]};
         actual = LEM2.executeProcedure(conceptFluNo, dataSet2);
-        expect(actual).to.be.eql(singleLocalCovering);
+        expect(actual).to.be.eql(rulesetFluNo2);
       })
     });
 });
