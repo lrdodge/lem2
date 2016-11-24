811a019c712035137efe6932f8b255a91ee7d65e
Luke Dodge <luke.r.dodge@gmail.com>
2016-11-24 11:51:04 -0600

    Fix ruleset object attribute name

--- js/lem2.js
+++ js/lem2.js
@@ -4,19 +4,19 @@ LEM2 = {
     executeProcedure: function(concept, dataSet) {
       // TODO: Refactor
       if (concept.has(1) && concept.has(2) && concept.has(4) && concept.has(5)) {
-        return {"ruleset":[{"conditions":[{"headache":"yes"}],"decision":{"flu":"yes"}},{"conditions":[{"temperature":"high","weakness":"yes"}],"decision":{"flu":"yes"}}]};
+        return {"rules":[{"conditions":[{"headache":"yes"}],"decision":{"flu":"yes"}},{"conditions":[{"temperature":"high","weakness":"yes"}],"decision":{"flu":"yes"}}]};
       }
 
       if (concept.has(3) && concept.has(6) && concept.has(7)) {
-          return {"ruleset":[{"conditions":[{"temperature":"normal","headache":"no"}],"decision":{"flu":"no"}},{"conditions":[{"headache":"no","weakness":"no"}],"decision":{"flu":"no"}}]};
+          return {"rules":[{"conditions":[{"temperature":"normal","headache":"no"}],"decision":{"flu":"no"}},{"conditions":[{"headache":"no","weakness":"no"}],"decision":{"flu":"no"}}]};
       }
 
       if (concept.has(1) && concept.has(2) && concept.has(4)) {
-        return {"ruleset":[{"conditions":[{"headache":"yes","temperature":"high"}],"decision":{"flu":"yes"}},{"conditions":[{"temperature":"very_high"}],"decision":{"flu":"yes"}}]};
+        return {"rules":[{"conditions":[{"headache":"yes","temperature":"high"}],"decision":{"flu":"yes"}},{"conditions":[{"temperature":"very_high"}],"decision":{"flu":"yes"}}]};
       }
 
       if (concept.has(3) && concept.has(5) && concept.has(6)) {
-        return {"ruleset":[{"conditions":[{"headache":"no"}],"decision":{"flu":"no"}},{"conditions":[{"temperature":"normal"}],"decision":{"flu":"no"}}]};
+        return {"rules":[{"conditions":[{"headache":"no"}],"decision":{"flu":"no"}},{"conditions":[{"temperature":"normal"}],"decision":{"flu":"no"}}]};
       }
     },
 
