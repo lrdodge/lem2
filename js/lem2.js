750894fa5994411edb3ed438385c91c505acdedb
Luke Dodge <luke.r.dodge@gmail.com>
2016-12-04 11:53:52 -0600

    Change function order

--- js/lem2.js
+++ js/lem2.js
@@ -5,30 +5,6 @@ LEM2 = {
     blocks: {},
     concepts: [],
 
-    // TODO: Refactor
-    executeProcedure: function (concept) {
-
-        if (concept.cases.has(1) && concept.cases.has(2) && concept.cases.has(4) && concept.cases.has(5)) {
-            var ruleset1 = [{ "conditions": [{ "attribute": "headache", "value": "yes" }], "decision": { "name": "flu", "value": "yes" } }, { "conditions": [{ "attribute": "temperature", "value": "high" }, { "attribute": "weakness", "value": "yes" }], "decision": { "name": "flu", "value": "yes" } }];
-            return LEM2.reduceRuleset(ruleset1);
-        }
-
-        if (concept.cases.has(3) && concept.cases.has(6) && concept.cases.has(7)) {
-            var ruleset2 = [{ "conditions": [{ "attribute": "temperature", "value": "normal" }, { "attribute": "headache", "value": "no" }], "decision": { "name": "flu", "value": "no" } }, { "conditions": [{ "attribute": "headache", "value": "no" }, { "attribute": "weakness", "value": "no" }], "decision": { "name": "flu", "value": "no" } }]
-            return LEM2.reduceRuleset(ruleset2);
-        }
-
-        if (concept.cases.has(1) && concept.cases.has(2) && concept.cases.has(4)) {
-            var ruleset3 = [{ "conditions": [{ "attribute": "headache", "value": "yes" }, { "attribute": "temperature", "value": "high" }], "decision": { "name": "flu", "value": "yes" } }, { "conditions": [{ "attribute": "temperature", "value": "very_high" }], "decision": { "name": "flu", "value": "yes" } }];
-            return LEM2.reduceRuleset(ruleset3);
-        }
-
-        if (concept.cases.has(3) && concept.cases.has(5) && concept.cases.has(6)) {
-            var ruleset4 = [{ "conditions": [{ "attribute": "headache", "value": "no" }], "decision": { "name": "flu", "value": "no" } }, { "conditions": [{ "attribute": "temperature", "value": "normal" }], "decision": { "name": "flu", "value": "no" } }];
-            return LEM2.reduceRuleset(ruleset4);
-        }
-    },
-
     newConcepts: function () {
         LEM2.concepts = [];
 
@@ -96,6 +72,30 @@ LEM2 = {
         });
     },
 
+    // TODO: Refactor
+    executeProcedure: function (concept) {
+
+        if (concept.cases.has(1) && concept.cases.has(2) && concept.cases.has(4) && concept.cases.has(5)) {
+            var ruleset1 = [{ "conditions": [{ "attribute": "headache", "value": "yes" }], "decision": { "name": "flu", "value": "yes" } }, { "conditions": [{ "attribute": "temperature", "value": "high" }, { "attribute": "weakness", "value": "yes" }], "decision": { "name": "flu", "value": "yes" } }];
+            return LEM2.reduceRuleset(ruleset1);
+        }
+
+        if (concept.cases.has(3) && concept.cases.has(6) && concept.cases.has(7)) {
+            var ruleset2 = [{ "conditions": [{ "attribute": "temperature", "value": "normal" }, { "attribute": "headache", "value": "no" }], "decision": { "name": "flu", "value": "no" } }, { "conditions": [{ "attribute": "headache", "value": "no" }, { "attribute": "weakness", "value": "no" }], "decision": { "name": "flu", "value": "no" } }]
+            return LEM2.reduceRuleset(ruleset2);
+        }
+
+        if (concept.cases.has(1) && concept.cases.has(2) && concept.cases.has(4)) {
+            var ruleset3 = [{ "conditions": [{ "attribute": "headache", "value": "yes" }, { "attribute": "temperature", "value": "high" }], "decision": { "name": "flu", "value": "yes" } }, { "conditions": [{ "attribute": "temperature", "value": "very_high" }], "decision": { "name": "flu", "value": "yes" } }];
+            return LEM2.reduceRuleset(ruleset3);
+        }
+
+        if (concept.cases.has(3) && concept.cases.has(5) && concept.cases.has(6)) {
+            var ruleset4 = [{ "conditions": [{ "attribute": "headache", "value": "no" }], "decision": { "name": "flu", "value": "no" } }, { "conditions": [{ "attribute": "temperature", "value": "normal" }], "decision": { "name": "flu", "value": "no" } }];
+            return LEM2.reduceRuleset(ruleset4);
+        }
+    },
+
     getCasesCoveredByRuleset: function (ruleset) {
 
         var coveredCases = new Set();
@@ -126,7 +126,7 @@ LEM2 = {
     reduceRuleset: function (ruleset) {
         var coveredCases = LEM2.getCasesCoveredByRuleset(ruleset);
         var reducedRuleset = [];
-        var removedRules = [];        
+        var removedRules = [];
 
         ruleset.forEach(function (rule, ruleIndex) {
             var rulesetMinusRule = ruleset.slice(0);
